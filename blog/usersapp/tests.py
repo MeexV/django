ef3efa009384b4471408c84b6fab3312ec267862
MaximV <meex21001@yandex.ru>
2024-06-09 22:03:31 +0700

    add tests

--- blog/usersapp/tests.py
+++ blog/usersapp/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from mixer.backend.django import mixer
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+ParsUser = get_user_model()
+
+class TestParsUser(TestCase):
+
+    def setUp(self):
+        self.user = mixer.blend(ParsUser, username='test_user', email='test@test.com')
+
+    def test_user_creation(self):
+        self.assertEqual(self.user.username, 'test_user')
+        self.assertEqual(self.user.email, 'test@test.com')
+
+    def test_email_unique(self):
+        with self.assertRaises(Exception):
+            mixer.blend(ParsUser, username='testuser2', email='test@test.com')
